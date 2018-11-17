0e0c1d8d2b749cf6b5574ec6537029cf29ccca3f
Diego Paiva <diego.paiva13@gmail.com>
2018-11-17 15:29:46 -0200

    Refatorando algumas partes importantes

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -1,40 +1,99 @@
 #ifndef GRAPH_H_INCLUDED
 #define GRAPH_H_INCLUDED
 
+#include <fstream>
 #include "Node.hpp"
 
 class Graph
 {
 public:
-  int order;
   std::vector<Node *> nodes;
   std::vector<std::vector<double>> arcs;
   std::vector<std::vector<double>> heuristics;
 
-  Graph() {};
-  ~Graph() {};
+  Graph(std::string instanceFile) { mount(instanceFile); }
 
-  Node* getNode(int id)
+  ~Graph() {}
+
+ /* Constrói os nós, arcos e heuristicas do grafo através do arquivo da
+  * instância passada como argumento
+  */
+  void mount(std::string instanceFile)
   {
-    return nodes.at(id);
-  };
+    int nodesAmount;
+    int arcsAmount;
+    int heuristicsAmount;
+    std::string id1;
+    std::string id2;
+    std::string weight;
+    std::string heuristic;
+    std::ifstream file(instanceFile);
+    std::streampos old;
+
+    if (!file.is_open())
+    {
+      std::cout << "Erro na leitura do arquivo" << std::endl;
+      exit(0);
+    }
+    else
+    {
+      // Eis a primeira linha
+      file >> nodesAmount;
+      file >> arcsAmount;
+      file >> heuristicsAmount;
+
+      nodes.resize(nodesAmount + 1);
+      arcs.resize(arcsAmount + 1);
+      heuristics.resize(heuristicsAmount + 1);
+
+      // Dummy node '0' pois os nós começam a partir de 1
+      addNode(0);
+
+      // Demais linhas
+      while (file >> id1 >> id2 >> weight && id1 != "heuristics")
+      {
+        if (!hasNode(atoi(id1.c_str())))
+        {
+          addNode(atoi(id1.c_str()));
+          heuristics.at(atoi(id1.c_str())).resize(heuristicsAmount + 1);
+        }
+
+        if (!hasNode(atoi(id2.c_str())))
+        {
+          addNode(atoi(id2.c_str()));
+          heuristics.at(atoi(id2.c_str())).resize(heuristicsAmount + 1);
+        }
+
+        arcs.at(atoi(id1.c_str())).resize(nodesAmount + 1);
+        addArc(atoi(id1.c_str()), atoi(id2.c_str()), atof(weight.c_str()));
+
+        // Salva posição da última linha lida
+        old = file.tellg();
+      }
+
+      // Retoma na linha que encontrou "heuristics"
+      file.seekg(old);
+
+      // Descarta o "heuristics"
+      file >> id1;
+
+      // Retoma a leitura dos valores (começam as heurísticas)
+      while (file >> id1 >> id2 >> heuristic)
+        addHeuristic(atoi(id1.c_str()), atoi(id2.c_str()), atof(heuristic.c_str()));
+    }
+  }
+
+  Node* getNode(int id) { return nodes.at(id); }
 
   void addNode(int id)
   {
     if (!hasNode(id))
-    {
       nodes.at(id) = new Node(id);
-    }
     else
-    {
       throw "Nó já existe no grafo!";
-    }
-  };
+  }
 
-  bool hasNode(int id)
-  {
-    return nodes.at(id) != NULL;
-  };
+  bool hasNode(int id) { return nodes.at(id) != NULL; }
 
   void addArc(int id1, int id2, double weight)
   {
@@ -47,42 +106,30 @@ public:
     {
       throw "Impossível adicionar arco - Um dos nós não existe no grafo";
     }
-  };
+  }
 
   double getArcWeight(Node *node1, Node *node2)
   {
     if (hasNode(node1->id) && hasNode(node2->id))
-    {
       return arcs.at(node1->id).at(node2->id);
-    }
     else
-    {
       throw "O(s) nó(s) não existe(m) no grafo de entrada";
-    }
   }
 
   void addHeuristic(int id1, int id2, double value)
   {
     if (hasNode(id1) && hasNode(id2))
-    {
       heuristics.at(id1).at(id2) = value;
-    }
     else
-    {
       throw "Impossível adicionar heurística - Um dos nós não existe no grafo";
-    }
-  };
+  }
 
   double getHeuristicValue(Node *node1, Node *node2)
   {
     if (hasNode(node1->id) && hasNode(node2->id))
-    {
       return heuristics.at(node1->id).at(node2->id);
-    }
     else
-    {
       throw "O(s) nó(s) não existe(m) no grafo de entrada";
-    }
   }
 
   std::list<Node *> getAdjacents(int id)
@@ -92,9 +139,7 @@ public:
     for (int i = 0; i < arcs.at(id).size(); i++)
     {
       if (arcs.at(id).at(i) != NULL)
-      {
         adjacents.push_back(getNode(i));
-      }
     }
 
     return adjacents;
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <fstream>
 #include "Algoritmos/searching/AStar.hpp"
 #include "Algoritmos/searching/Backtracking.hpp"
 #include "Algoritmos/searching/DepthFirstSearch.hpp"
@@ -6,103 +5,49 @@
 #include "Algoritmos/searching/UniformCostSearch.hpp"
 #include "Algoritmos/searching/GreedySearch.hpp"
 
-void createGraphFromFile(std::string file, Graph &graph)
-{
-  int nodesAmount;
-  int arcsAmount;
-  int heuristicsAmount;
-  std::string id1;
-  std::string id2;
-  std::string weight;
-  std::string heuristic;
-  std::ifstream f(file);
-  std::streampos old;
-
-  if (!f.is_open())
-  {
-    std::cout << "Erro na leitura do arquivo" << std::endl;
-    exit(0);
-  }
-  else
-  {
-    // Eis a primeira linha
-    f >> nodesAmount;
-    f >> arcsAmount;
-    f >> heuristicsAmount;
-    graph.order = nodesAmount;
-    graph.nodes.resize(nodesAmount + 1);
-    graph.arcs.resize(arcsAmount + 1);
-    graph.heuristics.resize(heuristicsAmount + 1);
-
-    // Dummy node pois os nós começam a partir de 1
-    graph.addNode(0);
-
-    // Demais linhas
-    while (f >> id1 >> id2 >> weight && id1 != "h")
-    {
-      if (!graph.hasNode(atoi(id1.c_str())))
-      {
-        graph.addNode(atoi(id1.c_str()));
-        graph.heuristics.at(atoi(id1.c_str())).resize(heuristicsAmount + 1);
-      }
-
-      if (!graph.hasNode(atoi(id2.c_str())))
-      {
-        graph.addNode(atoi(id2.c_str()));
-        graph.heuristics.at(atoi(id2.c_str())).resize(heuristicsAmount + 1);
-      }
-
-      graph.arcs.at(atoi(id1.c_str())).resize(nodesAmount + 1);
-      graph.addArc(atoi(id1.c_str()), atoi(id2.c_str()), atof(weight.c_str()));
-
-      // Salva posição da última linha lida
-      old = f.tellg();
-    }
-
-    // Retoma na linha que encontrou "h"
-    f.seekg(old);
-
-    // Descarta o "h"
-    f >> id1;
-
-    while (f >> id1 >> id2 >> heuristic)
-    {
-      graph.addHeuristic(atoi(id1.c_str()), atoi(id2.c_str()), atof(heuristic.c_str()));
-    }
-  }
-}
+#define EXPECTED_ARGUMENTS_AMOUNT 4
 
 int main(int argc, char* argv[])
 {
-  std::string file = argv[1];
+  if (argc != EXPECTED_ARGUMENTS_AMOUNT)
+  {
+    std::cout << "Número de argumentos inesperado! Saindo" << '\n';
+    exit(1);
+  }
+
+  std::string instanceFile = argv[1];
+
   int start = atoi(argv[2]);
   int end = atoi(argv[3]);
 
-  Graph *graph = new Graph();
-
-  createGraphFromFile(file, *graph);
+  Graph *graph = new Graph(instanceFile);
 
   Backtracking backtracking;
-  DepthFirstSearch dfs;
-  BreadthFirstSearch bfs;
-  UniformCostSearch ucs;
-  GreedySearch gs;
-  AStar as;
+  DepthFirstSearch depthFirstSearch;
+  BreadthFirstSearch breadthFirstSearch;
+  UniformCostSearch uniformCostSearch;
+  GreedySearch greedySearch;
+  AStar aStar;
 
   try
   {
     std::cout << "Backtracking - Solução: ";
     backtracking.printPath(graph, start, end);
+
     std::cout << "BFS - Solução: ";
-    bfs.printPath(graph, start, end);
+    breadthFirstSearch.printPath(graph, start, end);
+
     std::cout << "DFS - Solução: ";
-    dfs.printPath(graph, start, end);
+    depthFirstSearch.printPath(graph, start, end);
+
     std::cout << "Ordenada - Solução: ";
-    ucs.printPath(graph, start, end);
+    uniformCostSearch.printPath(graph, start, end);
+
     std::cout << "Gulosa - Solução: ";
-    gs.printPath(graph, start, end);
+    greedySearch.printPath(graph, start, end);
+
     std::cout << "A* - Solução: ";
-    as.printPath(graph, start, end);
+    aStar.printPath(graph, start, end);
   }
   catch (char const* exception)
   {
